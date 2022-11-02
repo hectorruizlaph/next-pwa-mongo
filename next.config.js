66b143bbd90a4e40c53d09821633a03f144f616e
Hector Ruiz <hectorruizlaph@gmail.com>
2022-11-02 12:20:19 -0700

    all working

--- next.config.js
+++ next.config.js
@@ -1,15 +1,17 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')
+
+const runtimeCaching = require('next-pwa/cache')
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
+})
 
 const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
-  pwa: {
-    dest: 'public',
-    registes: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
-  },
 })
 
 module.exports = nextConfig
